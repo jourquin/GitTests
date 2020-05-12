e423acb1e0e42f64a2cc2cf85bf74eba5ba1e02e
Bart Jourquin <bart.jourquin@uclouvain.be>
2020-05-12 16:19:42 +0200

    zzz

--- src/tests/module/Module1.java
+++ src/tests/module/Module1.java
@@ -3,7 +3,7 @@ package tests.module;
 public class Module1 {
 
   public Module1() {
-   System.out.println("Version 6");
-   System.out.println("This is a change in master"); 
+   System.out.println("Version 7");
+   System.out.println("blabla"); 
   }
 }
