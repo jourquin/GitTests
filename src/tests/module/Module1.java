b0af6f05ab1a4fbf3899cacd9146b5e17dc4fafb
Bart Jourquin <bart.jourquin@uclouvain.be>
2020-05-12 15:24:51 +0200

    Change in master

--- src/tests/module/Module1.java
+++ src/tests/module/Module1.java
@@ -4,6 +4,6 @@ public class Module1 {
 
   public Module1() {
    System.out.println("Version 4");
-   System.out.println("new lineeeeeeex");
+   System.out.println("This is a change in master");
   }
 }
