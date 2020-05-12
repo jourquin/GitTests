9da49c5c0da37b9a680eed00d67159dde89e7ae4
Bart Jourquin <bart.jourquin@uclouvain.be>
2020-05-12 17:12:34 +0200

    v11

--- src/tests/module/Module1.java
+++ src/tests/module/Module1.java
@@ -3,7 +3,7 @@ package tests.module;
 public class Module1 {
 
   public Module1() {
-   System.out.println("Version 10");
-   System.out.println("blabla 10"); 
+   System.out.println("Version 11");
+   System.out.println("blabla 111"); 
   }
 }
